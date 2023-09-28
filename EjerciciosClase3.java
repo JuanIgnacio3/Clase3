2428539c73bb3a3cdc1b00febdd8123b9a01e1ac
JuanIgnacio3 <145624612+JuanIgnacio3@users.noreply.github.com>
2023-09-27 20:18:47 -0600

    Add files via upload

---
+++ EjerciciosClase3.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicios.clase.pkg3;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author LABORATORIO 04
+ */
+public class EjerciciosClase3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JOptionPane 
+       /* //EJERCICIO 1
+        String resultado ="";
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 5; j++) {
+                resultado = resultado + "@";
+            }
+            resultado = resultado + "\n";
+        }
+        System.out.println(resultado);*/
+       
+       //EJERCICIO 2
+       String num= "";
+       num = JOptionPane.showInputDialog("Digite un número: ");       
+       
+       
+}
+
+       
+    }
+    
+}
